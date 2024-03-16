8499cd7ee1c9a615b5835f9876eb8298c7d20e82
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-03-16 10:53:17 +0700

    Allow invoking build script from anywhere

--- README.md
+++ README.md
@@ -2,8 +2,5 @@
 Lightweight Windows command shell
 
 ## Build instructions
-A C++17 compiler (in Windows, of course) is required. To build the source files, navigate to the root of the repository and run
-```bash
->scripts\build
-```
+A C++17 compiler (in Windows, of course) is required. To build the source files, run [scripts/build.bat](scripts/build.bat)
 This batch script will build the executables under the `build/` directory, which contains the command shell `shell.exe`.
--- scripts/build.bat
+++ scripts/build.bat
@@ -1,17 +1,20 @@
 @echo off
 g++ --version
 
-if not exist build\ mkdir build
-set params=-O3 -Wall -I src\include -std=c++17
+for %%f in ("%~dp0..") do set root=%%~ff
+echo Got root of repository: %root%
 
-echo Building shell.cpp
-g++ %params% src\shell.cpp -o build\shell.exe
+if not exist %root%\build mkdir %root%\build
+set params=-O3 -Wall -I %root%\src\include -std=c++17
+
+echo Building %root%\src\shell.cpp to %root%\build\shell.exe
+g++ %params% %root%\src\shell.cpp -o %root%\build\shell.exe
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-for %%f in (src\external\*) do (
+for %%f in (%root%\src\external\*) do (
     if /i %%~xf equ .cpp (
-        echo Building %%f to %%~nf.exe
-        g++ %params% %%f -o build\%%~nf.exe
+        echo Building %%f to %root%\build\%%~nf.exe
+        g++ %params% %%f -o %root%\build\%%~nf.exe
         if %errorlevel% neq 0 exit /b %errorlevel%
     )
 )
