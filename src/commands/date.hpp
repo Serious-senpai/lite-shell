c5e43cdc3453a9e35badb0d5ffd88127885026b2
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-03-18 11:24:24 +0700

    Display local time using "date"

--- src/commands/date.hpp
+++ src/commands/date.hpp
@@ -19,10 +19,15 @@ public:
     DWORD run(const Context &context)
     {
         SYSTEMTIME time;
-        GetSystemTime(&time);
 
-        std::cout << "System time:\n"
-                  << __week_days[time.wDayOfWeek] << " " << time.wDay << "/" << time.wMonth << "/" << time.wYear << "\n"
+        GetSystemTime(&time);
+        std::cout << "System time (UTC): "
+                  << __week_days[time.wDayOfWeek] << " " << time.wDay << "/" << time.wMonth << "/" << time.wYear << " "
+                  << time.wHour << ":" << time.wMinute << ":" << time.wSecond << std::endl;
+
+        GetLocalTime(&time);
+        std::cout << "Local time: "
+                  << __week_days[time.wDayOfWeek] << " " << time.wDay << "/" << time.wMonth << "/" << time.wYear << " "
                   << time.wHour << ":" << time.wMinute << ":" << time.wSecond << std::endl;
 
         return 0;
